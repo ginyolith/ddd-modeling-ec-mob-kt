48d81e2f6c9d463947cb4c409c25b982376c69d0
Fumiya.Akagi <fumiya.akagi@iridge.jp>
2019-04-13 14:44:32 +0900

    add : RestAPIを意識してECアプリケーションのインターフェイスを実装及びテスト追加

---
+++ src/main/kotlin/CommerceApplication.kt
@@ -0,0 +1,25 @@
+
+object CommerceApplication {
+
+    /**
+     * カートに商品を追加する
+     * @param productCode 商品コード
+     */
+    fun addToCart(vararg productCode : String) : Boolean{
+        TODO("not implemented")
+    }
+
+    /**
+     * カート項目の一覧を表示する
+     */
+    fun showCartItems() : String {
+        TODO("not implemented")
+    }
+
+    /**
+     * 発注書を生成する
+     */
+    fun createPurchaseOrderSheet() : String {
+        TODO("not implemented")
+    }
+}
