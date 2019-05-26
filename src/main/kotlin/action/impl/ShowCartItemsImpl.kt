fba5ad1aba0768578b9b94781e1a7ac31b05cb6d
Fumiya.Akagi <fumiya.akagi@iridge.jp>
2019-05-26 13:36:34 +0900

    add : #18 UseCaseのIFを変更

--- src/main/kotlin/action/impl/ShowCartItemsImpl.kt
+++ src/main/kotlin/action/impl/ShowCartItemsImpl.kt
@@ -1,10 +1,11 @@
 package action.impl
 
 import action.ShowCartItems
+import shopping.model.cart.Cart
 import shopping.repository.CartRepository
 
 class ShowCartItemsImpl(private val cartRepository: CartRepository) : ShowCartItems {
-    override fun execute(input: Unit): String {
-        return cartRepository.cart.showCartItems()
+    override fun execute(input: Unit): Cart {
+        return cartRepository.cart
     }
 }
