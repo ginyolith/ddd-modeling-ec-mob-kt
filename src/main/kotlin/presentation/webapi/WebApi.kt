b37ab777617edaafff99d2191d95fb1498a31b7c
naokiur <atomic.transfer.comp@gmail.com>
2019-06-08 14:37:00 +0900

    mod: #21 テストを実施しやすくするために、main()によるembeddedServer の実行ではなく、io.ktor.server.netty.EngineMainによる実行に変更

--- src/main/kotlin/presentation/webapi/WebApi.kt
+++ src/main/kotlin/presentation/webapi/WebApi.kt
@@ -2,6 +2,7 @@ package presentation.webapi
 
 import com.squareup.moshi.Moshi
 import di.UseCaseInjection
+import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
@@ -16,7 +17,7 @@ import presentation.webapi.json.JsonCartItem
 import presentation.webapi.json.JsonCartItems
 
 
-fun main(args: Array<String>) {
+fun Application.main() {
     // カートへの追加処理
     val addToCart = UseCaseInjection.addToCart
 
@@ -26,41 +27,37 @@ fun main(args: Array<String>) {
     // 発注書を生成する処理
     val createPurchaseOrderSheet = UseCaseInjection.createPurchaseOrderSheet
 
+    routing {
+        get("/") {
+            call.respondText("Hello World!", ContentType.Text.Plain)
+        }
+        get("/v1/cart/show") {
 
-    val server = embeddedServer(Netty, port = 8080) {
-        routing {
-            get("/") {
-                call.respondText("Hello World!", ContentType.Text.Plain)
-            }
-            get("/v1/cart/show") {
-
-                val cart: JsonCartItems = showCartItems.execute(Unit).let {
-                    val jsonCartItemList = it.items.map {
-                        JsonCartItem(
-                                productName = it.product.productName.value,
-                                unitPrice = it.product.unitPrice.value,
-                                quantity = it.quantity.value,
-                                totalPrice = it.小計.value
-                        )
-                    }
-
-                    JsonCartItems(jsonCartItemList, it.小計.value)
+            val cart: JsonCartItems = showCartItems.execute(Unit).let {
+                val jsonCartItemList = it.items.map {
+                    JsonCartItem(
+                            productName = it.product.productName.value,
+                            unitPrice = it.product.unitPrice.value,
+                            quantity = it.quantity.value,
+                            totalPrice = it.小計.value
+                    )
                 }
 
-                val moshi = Moshi.Builder().build()
-                val json = moshi.adapter(JsonCartItems::class.java).toJson(cart)
-
-                call.respondText(json, ContentType.Application.Json, status = HttpStatusCode.OK)
+                JsonCartItems(jsonCartItemList, it.小計.value)
             }
-            post("/v1/cart/add") {
-                val param = requireNotNull(call.request.header("productCodes"))
-                val productCodes = param.split(",")
 
-                addToCart.execute(productCodes)
+            val moshi = Moshi.Builder().build()
+            val json = moshi.adapter(JsonCartItems::class.java).toJson(cart)
 
-                call.respondText("Success", ContentType.Text.Plain, status = HttpStatusCode.OK)
-            }
+            call.respondText(json, ContentType.Application.Json, status = HttpStatusCode.OK)
+        }
+        post("/v1/cart/add") {
+            val param = requireNotNull(call.request.header("productCodes"))
+            val productCodes = param.split(",")
+
+            addToCart.execute(productCodes)
+
+            call.respondText("Success", ContentType.Text.Plain, status = HttpStatusCode.OK)
         }
     }
-    server.start(wait = true)
 }
